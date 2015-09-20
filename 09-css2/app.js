86c7b714484fa8485a9646dcc0751739f54bd48a
benhassara <bhassara@gmail.com>
2015-09-19 20:26:24 -0600

    finish 10 event

--- 09-css2/app.js
+++ 09-css2/app.js
@@ -1,7 +1,5 @@
 var React = require('react');
 var TodoBox = require('./views/index.jsx');
 
-var data = JSON.parse(document.getElementById('intial-data')
-                              .getAttribute('data-json'));
-
-React.render(<TodoBox data={data} />, document.getElementById('app'));
+var data = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
+React.render(<TodoBox data={data} />, document.getElementById("app"));
